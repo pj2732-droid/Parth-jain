ca397fb29165fcdd17f3af0013d1810ba0ddbabe
PARTH JAIN <parthjain@PARTHs-MacBook-Air-5.local>
2026-02-16 19:28:42 +0530

    UC2 Print Banner

---
+++ PrintBanner.java
@@ -0,0 +1,14 @@
+public class PrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   *****   ***** ");
+        System.out.println("*     *  *     * *     * *     *");
+        System.out.println("*     *  *     * *     * *      ");
+        System.out.println("*     *  *     * *****   *****  ");
+        System.out.println("*     *  *     * *            * ");
+        System.out.println("*     *  *     * *            * ");
+        System.out.println(" *****    *****  *       *****  ");
+
+    }
+}
